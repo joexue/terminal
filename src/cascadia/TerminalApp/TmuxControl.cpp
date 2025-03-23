8a86e4beeb0c9386b8e8f7be4fedfb187f5589e9
Joe Xue <lgxue@hotmail.com>
2025-03-22 23:05:33 -0400

    Eventually !
    	modified:   src/cascadia/TerminalApp/TmuxControl.cpp
    	modified:   src/cascadia/TerminalApp/TmuxControl.h

--- src/cascadia/TerminalApp/TmuxControl.cpp
+++ src/cascadia/TerminalApp/TmuxControl.cpp
@@ -4,48 +4,125 @@
 #include "pch.h"
 #include "TmuxControl.h"
 
-//using namespace winrt::Microsoft::Terminal::Control::TermControl;
-
 namespace winrt::TerminalApp::implementation
 {
+    const std::wregex TmuxControl::REG_BEGIN{ L"^%begin \\d+ \\d+ \\d+$" };
+    const std::wregex TmuxControl::REG_END{ L"^%end \\d+ \\d+ \\d+$" };
+    const std::wregex TmuxControl::REG_ERROR{ L"^%error \\d+ \\d+ \\d+$" };
+    const std::wregex TmuxControl::REG_SESSION_CHANGED{ L"^%session-changed \\$\\d+ \\w+$" };
+    const std::wregex TmuxControl::REG_EXIT{ L"^%exit$" };
+
     TmuxControl::TmuxControl(std::shared_ptr<Pane> pane):
         _pane(pane)
     {
         auto _core = _pane->GetTerminalControl();
 
         _core.SetTmuxControlHandlerProducer([this]() {
-                _state = State::ATTACHING;
+            _state = State::ATTACHING;
+            _buffer.clear();
 
-                return [this](const auto ch) mutable {
-                    if (ch == '\n') {
-                        _pane->GetTerminalControl().LineFeed();
-                    } else {
-                        _pane->GetTerminalControl().Print(ch);
-                    }
-                    /*
-                    bool ret = _tmuxDcsHandler ? _tmuxDcsHandler(ch) : false;
-                    if (!ret) {
-                        _isTmux = false;
-                        _sendInputToConnection(L"\n");
-                    }
-                    return ret;
-                    */
-                    return true;
-                };
+            return [this](const auto ch) mutable {
+                return _advance(ch);
+            };
         });
-        _core.SetTmuxKeyHandler([this, _core](const auto ch) mutable {
-            if (_state != INIT)
-            {
-                if (ch == 'q' || ch == 'Q')
-                {
-                    _core.RawWriteString(L"detach\n");
-                }
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+        _core.SetTmuxKeyHandler([this](const auto ch) mutable {
+            return _keyDown(ch);
         });
+    }
+
+    bool TmuxControl::_eventHandle(Event& e)
+    {
+        switch (e.type)
+        {
+        case EXIT:
+            auto _core = _pane->GetTerminalControl();
+            _core.RawWriteString(L"\n\n");
+            break;
+        }
+        return true;
+    }
+
+    bool TmuxControl::_parse()
+    {
+        Event e;
+
+        std::wstring line(_buffer.begin(), _buffer.end());
+
+        if (std::regex_match(line, REG_BEGIN))
+        {
+            e.type = BEGIN;
+        }
+        else if (std::regex_match(line, REG_END))
+        {
+            e.type = END;
+        }
+        else if (std::regex_match(line, REG_ERROR))
+        {
+            e.type = ERR;
+        }
+        else if (std::regex_match(line, REG_EXIT))
+        {
+            e.type = EXIT;
+        }
+        else if (std::regex_match(line, REG_SESSION_CHANGED))
+        {
+            e.type = SESSION_CHANGED;
+        }
+        else //either we send \n to let tmux control exit or exception such as ssh broken connection
+        {
+            _state = INIT;
+            return false;
+        }
+
+        return _eventHandle(e);
+    }
+
+    bool TmuxControl::_advance(wchar_t ch)
+    {
+        bool res;
+
+        //TODO: remove them
+        if (ch == '\n')
+        {
+            _pane->GetTerminalControl().LineFeed();
+        }
+        else
+        {
+            _pane->GetTerminalControl().Print(ch);
+        }
+
+        if (ch == '\n')
+        {
+            res = _parse();
+            _buffer.clear();
+        }
+        else if (ch == '\r')
+        {
+            res = true;
+        }
+        else
+        {
+            _buffer.push_back(ch);
+            res = true;
+        }
+        return res;
+    }
+
+    bool TmuxControl::_keyDown(wchar_t ch)
+    {   
+        if (_state != INIT)
+        {
+            // Only accept 'q' in tmux control pane
+            if (ch == 'q')
+            {
+                auto _core = _pane->GetTerminalControl();
+                _core.RawWriteString(L"detach\n\n\n");
+            }
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
--- src/cascadia/TerminalApp/TmuxControl.h
+++ src/cascadia/TerminalApp/TmuxControl.h
@@ -3,23 +3,53 @@
 
 #pragma once
 
+#include <regex>
+
+#include <vector>
 #include "Pane.h"
 
 namespace winrt::TerminalApp::implementation
 {
     class TmuxControl
     {
-        enum State : int {
-            INIT = 0,
-            ATTACHING = 1,
-        };
     public:
         TmuxControl(std::shared_ptr<Pane> pane);
 
-        winrt::Microsoft::Terminal::Control::TermControl* _control { nullptr };
-        //static bool DcsHandler(TmuxControl *This, wch const wchar_t);
     private:
+        static const std::wregex REG_BEGIN;
+        static const std::wregex REG_END;
+        static const std::wregex REG_ERROR;
+
+        static const std::wregex REG_SESSION_CHANGED;
+        static const std::wregex REG_EXIT;
+
+        enum State : int
+        {
+            INIT = 0,
+            ATTACHING = 1,
+        } _state{ INIT };
+
+        enum EventType : int
+        {
+            BEGIN,
+            END,
+            ERR,
+            ENTER,
+            EXIT,
+            SESSION_CHANGED,
+        } _event;
+
+        struct Event
+        {
+            EventType type;
+        };
+
+        bool _eventHandle(Event &e);
+        bool _advance(wchar_t ch);
+        bool _parse();
+        bool _keyDown(wchar_t ch);
+
         std::shared_ptr<Pane> _pane { nullptr };
-        State _state { State::INIT };
+        std::vector<wchar_t> _buffer;
     };
 }
